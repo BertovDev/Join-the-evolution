e2aab4f514621913c6ea240c6d59069cf3d155dc
BertovDev <bertobautista18@gmail.com>
2024-11-24 03:12:11 -0300

    Add final controls and shader test

--- src/components/3D/Cube.tsx
+++ src/components/3D/Cube.tsx
@@ -1,6 +1,10 @@
 import { MeshProps, useFrame } from "@react-three/fiber";
-import React, { createRef, useRef } from "react";
-import { Mesh } from "three";
+import React, { createRef, useMemo, useRef } from "react";
+import { Color, Mesh } from "three";
+import vertexShader from "../../shaders/vertex.glsl";
+import fragmentShader from "../../shaders/fragment.glsl";
+import { useControls } from "leva";
+import * as THREE from "three";
 
 interface CubeProps {
   gestures: string | undefined;
@@ -9,20 +13,66 @@ interface CubeProps {
 export default function Cube({ gestures }: CubeProps) {
   const cubeRef = useRef<Mesh>(null!);
 
-  useFrame(() => {
+  const { amplitude, frequency, colorB } = useControls({
+    amplitude: {
+      value: -0.2,
+    },
+    frequency: {
+      value: 1,
+    },
+    colorB: {
+      value: new THREE.Color("#9c1f32"),
+    },
+  });
+
+  const uniforms = useMemo(
+    () => ({
+      uTime: {
+        value: 0,
+      },
+      uColorA: {
+        value: new THREE.Color("#FFE486"),
+      },
+      uColorB: {
+        value: new THREE.Color("#030303"),
+      },
+      uAmplitude: {
+        value: amplitude,
+      },
+      uFrequency: {
+        value: frequency,
+      },
+    }),
+    []
+  );
+
+  useFrame((state) => {
     if (cubeRef.current) {
       if (gestures === "Open_Palm") {
         cubeRef.current.position.z -= 0.11;
       } else if (gestures === "Closed_Fist") {
-        cubeRef.current.position.z += 0.11;
+        uniforms.uFrequency.value += 0.04;
       }
     }
+
+    // uniforms.uFrequency.value = frequency;
+    uniforms.uAmplitude.value = amplitude;
+    // uniforms.uColorB.value = colorB;
   });
 
   return (
-    <mesh position={[0, 0, 0]} ref={cubeRef}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color="white" />
+    <mesh
+      position={[0, 0, 0]}
+      rotation={[-Math.PI / 2, 0, 0]}
+      scale={1}
+      ref={cubeRef}
+    >
+      <icosahedronGeometry args={[1, 32]} />
+      <shaderMaterial
+        fragmentShader={fragmentShader}
+        vertexShader={vertexShader}
+        uniforms={uniforms}
+      />
     </mesh>
   );
 }
